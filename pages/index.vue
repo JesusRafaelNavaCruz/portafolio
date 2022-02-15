e6850df84543054cc143b9e85eb306cfaf9fcd83
Jesus Rafael Nava Cruz <dev10@erillamhc.com>
2022-02-14 21:25:21 -0600

    Modificaciones en contenido, agrega seccion de contacto

--- pages/index.vue
+++ pages/index.vue
@@ -39,7 +39,7 @@
             >
               Portafolio
             </a>
-            <!-- <a class="btn btn-ghost btn-sm rounded-btn"> Contact </a> -->
+            <a class="btn btn-ghost btn-sm rounded-btn" v-scroll-to="'#contact_section'"> Contact </a>
           </div>
         </div>
         <div class="navbar-end">
@@ -59,10 +59,10 @@
                 shadow
                 menu
                 dropdown-content
-                bg-base-content
                 text-bg-primary
                 rounded-box
                 w-52
+                glass
               "
             >
               <li v-for="(theme, index) in themes" :key="index">
@@ -95,7 +95,7 @@
             </p>
             <a
               class="btn btn-primary mr-5"
-              href="/"
+              href="/curriculum"
               download="cv_Jesus_Rafael_Nava_Cruz.pdf"
               >Descargar CV</a
             >
@@ -342,7 +342,7 @@
           <h1 class="text-4xl font-bold pt-10 text-primary" id="about_section">
             Portafolio
           </h1>
-          <div class="divider w-48 mt-0"></div>
+          <div class="bg-primary h-1 w-48 mt-0 mb-10"></div>
         </div>
 
         <div
@@ -382,8 +382,90 @@
                   {{ tech }}
                 </div>
               </div>
+
+              <div
+                v-for="(url, i) in project.urls"
+                :key="i"
+                class="flex flex-wrap items-end mt-4"
+              >
+                <a :href="url.web" target="_blank" v-if="url.web">
+                  <svg style="width: 40px; height: 40px" viewBox="0 0 24 24">
+                    <path
+                      fill="currentColor"
+                      d="M16.36,14C16.44,13.34 16.5,12.68 16.5,12C16.5,11.32 16.44,10.66 16.36,10H19.74C19.9,10.64 20,11.31 20,12C20,12.69 19.9,13.36 19.74,14M14.59,19.56C15.19,18.45 15.65,17.25 15.97,16H18.92C17.96,17.65 16.43,18.93 14.59,19.56M14.34,14H9.66C9.56,13.34 9.5,12.68 9.5,12C9.5,11.32 9.56,10.65 9.66,10H14.34C14.43,10.65 14.5,11.32 14.5,12C14.5,12.68 14.43,13.34 14.34,14M12,19.96C11.17,18.76 10.5,17.43 10.09,16H13.91C13.5,17.43 12.83,18.76 12,19.96M8,8H5.08C6.03,6.34 7.57,5.06 9.4,4.44C8.8,5.55 8.35,6.75 8,8M5.08,16H8C8.35,17.25 8.8,18.45 9.4,19.56C7.57,18.93 6.03,17.65 5.08,16M4.26,14C4.1,13.36 4,12.69 4,12C4,11.31 4.1,10.64 4.26,10H7.64C7.56,10.66 7.5,11.32 7.5,12C7.5,12.68 7.56,13.34 7.64,14M12,4.03C12.83,5.23 13.5,6.57 13.91,8H10.09C10.5,6.57 11.17,5.23 12,4.03M18.92,8H15.97C15.65,6.75 15.19,5.55 14.59,4.44C16.43,5.07 17.96,6.34 18.92,8M12,2C6.47,2 2,6.5 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z"
+                    />
+                  </svg>
+                </a>
+                <a :href="url.repo" target="_blank" v-if="url.repo">
+                  <svg style="width: 40px; height: 40px" viewBox="0 0 24 24">
+                    <path
+                      fill="currentColor"
+                      d="M2.6,10.59L8.38,4.8L10.07,6.5C9.83,7.35 10.22,8.28 11,8.73V14.27C10.4,14.61 10,15.26 10,16A2,2 0 0,0 12,18A2,2 0 0,0 14,16C14,15.26 13.6,14.61 13,14.27V9.41L15.07,11.5C15,11.65 15,11.82 15,12A2,2 0 0,0 17,14A2,2 0 0,0 19,12A2,2 0 0,0 17,10C16.82,10 16.65,10 16.5,10.07L13.93,7.5C14.19,6.57 13.71,5.55 12.78,5.16C12.35,5 11.9,4.96 11.5,5.07L9.8,3.38L10.59,2.6C11.37,1.81 12.63,1.81 13.41,2.6L21.4,10.59C22.19,11.37 22.19,12.63 21.4,13.41L13.41,21.4C12.63,22.19 11.37,22.19 10.59,21.4L2.6,13.41C1.81,12.63 1.81,11.37 2.6,10.59Z"
+                    />
+                  </svg>
+                </a>
+              </div>
             </div>
           </div>
+        </div>
+      </div>
+
+      <div class="container" id="contact_section">
+        <div class="text-center flex flex-col justify-center items-center">
+          <h1 class="text-4xl font-bold pt-10 text-primary" id="about_section">
+            Contacto
+          </h1>
+          <div class="bg-primary h-1 w-48 mt-0 mb-10"></div>
+        </div>
+
+        <div class="text-center">
+          <h1 class="text-3xl text-primary font-bold mb-5">
+            ¿Necesitas ayuda para tus proyectos?
+          </h1>
+          <p class="text-lg text-primary font-semibold">
+            ¡Buscas un desarrollador front-end para construir tus aplicaciones o
+            sitios web?
+          </p>
+          <p class="text-lg text-primary font-semibold mb-5">
+            Puedes contactarme por
+            <span class="font-bold">correo electrónico</span>,
+            <span class="font-bold">redes sociales</span> y
+            <span class="font-bold">Whatsapp</span> que te dejo aqui abajo
+          </p>
+        </div>
+        <div class="flex flex-wrap items-center justify-center">
+          <a href="mailto:jrnc9802@gmail.com" target="_blank" class="text-error mx-5">
+            <svg style="width: 50px; height: 50px" viewBox="0 0 24 24">
+              <path
+                fill="currentColor"
+                d="M20,18H18V9.25L12,13L6,9.25V18H4V6H5.2L12,10.25L18.8,6H20M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4Z"
+              />
+            </svg>
+          </a>
+          <a href="https://twitter.com/el_rafinia" target="_blank" class="text-info mx-5">
+            <svg style="width: 50px; height: 50px" viewBox="0 0 24 24">
+              <path
+                fill="currentColor"
+                d="M22.46,6C21.69,6.35 20.86,6.58 20,6.69C20.88,6.16 21.56,5.32 21.88,4.31C21.05,4.81 20.13,5.16 19.16,5.36C18.37,4.5 17.26,4 16,4C13.65,4 11.73,5.92 11.73,8.29C11.73,8.63 11.77,8.96 11.84,9.27C8.28,9.09 5.11,7.38 3,4.79C2.63,5.42 2.42,6.16 2.42,6.94C2.42,8.43 3.17,9.75 4.33,10.5C3.62,10.5 2.96,10.3 2.38,10C2.38,10 2.38,10 2.38,10.03C2.38,12.11 3.86,13.85 5.82,14.24C5.46,14.34 5.08,14.39 4.69,14.39C4.42,14.39 4.15,14.36 3.89,14.31C4.43,16 6,17.26 7.89,17.29C6.43,18.45 4.58,19.13 2.56,19.13C2.22,19.13 1.88,19.11 1.54,19.07C3.44,20.29 5.7,21 8.12,21C16,21 20.33,14.46 20.33,8.79C20.33,8.6 20.33,8.42 20.32,8.23C21.16,7.63 21.88,6.87 22.46,6Z"
+              />
+            </svg>
+          </a>
+          <a href="https://www.instagram.com/el.rafinia/?hl=es-la" target="_blank" class="text-pink-600 mx-5">
+            <svg style="width: 50px; height: 50px" viewBox="0 0 24 24">
+              <path
+                fill="currentColor"
+                d="M7.8,2H16.2C19.4,2 22,4.6 22,7.8V16.2A5.8,5.8 0 0,1 16.2,22H7.8C4.6,22 2,19.4 2,16.2V7.8A5.8,5.8 0 0,1 7.8,2M7.6,4A3.6,3.6 0 0,0 4,7.6V16.4C4,18.39 5.61,20 7.6,20H16.4A3.6,3.6 0 0,0 20,16.4V7.6C20,5.61 18.39,4 16.4,4H7.6M17.25,5.5A1.25,1.25 0 0,1 18.5,6.75A1.25,1.25 0 0,1 17.25,8A1.25,1.25 0 0,1 16,6.75A1.25,1.25 0 0,1 17.25,5.5M12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9Z"
+              />
+            </svg>
+          </a>
+          <a href="https://wa.me/7772670869" target="_blank" class="text-green-500 mx-5">
+            <svg style="width: 50px; height: 50px" viewBox="0 0 24 24">
+              <path
+                fill="currentColor"
+                d="M12.04 2C6.58 2 2.13 6.45 2.13 11.91C2.13 13.66 2.59 15.36 3.45 16.86L2.05 22L7.3 20.62C8.75 21.41 10.38 21.83 12.04 21.83C17.5 21.83 21.95 17.38 21.95 11.92C21.95 9.27 20.92 6.78 19.05 4.91C17.18 3.03 14.69 2 12.04 2M12.05 3.67C14.25 3.67 16.31 4.53 17.87 6.09C19.42 7.65 20.28 9.72 20.28 11.92C20.28 16.46 16.58 20.15 12.04 20.15C10.56 20.15 9.11 19.76 7.85 19L7.55 18.83L4.43 19.65L5.26 16.61L5.06 16.29C4.24 15 3.8 13.47 3.8 11.91C3.81 7.37 7.5 3.67 12.05 3.67M8.53 7.33C8.37 7.33 8.1 7.39 7.87 7.64C7.65 7.89 7 8.5 7 9.71C7 10.93 7.89 12.1 8 12.27C8.14 12.44 9.76 14.94 12.25 16C12.84 16.27 13.3 16.42 13.66 16.53C14.25 16.72 14.79 16.69 15.22 16.63C15.7 16.56 16.68 16.03 16.89 15.45C17.1 14.87 17.1 14.38 17.04 14.27C16.97 14.17 16.81 14.11 16.56 14C16.31 13.86 15.09 13.26 14.87 13.18C14.64 13.1 14.5 13.06 14.31 13.3C14.15 13.55 13.67 14.11 13.53 14.27C13.38 14.44 13.24 14.46 13 14.34C12.74 14.21 11.94 13.95 11 13.11C10.26 12.45 9.77 11.64 9.62 11.39C9.5 11.15 9.61 11 9.73 10.89C9.84 10.78 10 10.6 10.1 10.45C10.23 10.31 10.27 10.2 10.35 10.04C10.43 9.87 10.39 9.73 10.33 9.61C10.27 9.5 9.77 8.26 9.56 7.77C9.36 7.29 9.16 7.35 9 7.34C8.86 7.34 8.7 7.33 8.53 7.33Z"
+              />
+            </svg>
+          </a>
         </div>
       </div>
     </section>
@@ -544,6 +626,7 @@ export default {
             "Git",
             "Bootstrap",
           ],
+          urls: [{ web: "https://www.ittec.mx/", repo: "" }],
         },
         {
           title: "SUIM",
@@ -558,36 +641,39 @@ export default {
             "Axios",
             "Git",
           ],
+          urls: [{ web: "https://erillamhc.com/", repo: "" }],
         },
         {
           title: "Portafolio",
           description: "Creación de portafolio personal",
           technology: ["HTML", "CSS", "Vuejs", "Nuxt", "Javascript", "DaisyUI"],
+          urls: [
+            {
+              web: "https://jesusrafaelnavacruz.vercel.app/",
+              repo: "https://github.com/JesusRafaelNavaCruz/portafolio",
+            },
+          ],
+        },
+        {
+          title: "Calculadora",
+          description: "Calculadora con funciones basicas ",
+          technology: ["HTML", "CSS", "Vuejs", "Javascript"],
+          urls: [
+            { web: "https://determined-meitner-b8cd1f.netlify.app", repo: "" },
+          ],
         },
       ],
       themes: [
         "light",
         "dark",
-        "cupcake",
         "bumblebee",
         "emerald",
-        "corporate",
-        "synthwave",
-        "retro",
-        "cyberpunk",
         "valentine",
         "halloween",
-        "garden",
-        "forest",
-        "aqua",
-        "lofi",
-        "pastel",
         "fantasy",
-        "wireframe",
         "black",
         "luxury",
         "dracula",
-        "cmyk",
       ],
     };
   },
