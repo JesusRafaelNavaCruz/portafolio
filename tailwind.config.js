66f50bfd57fb51ee7c47962c7978a36c97e1fb09
Jesus Rafael Nava Cruz <jesus.nava.mx@cloudappi.net>
2022-08-26 18:52:45 -0500

    Modify content, add breakpoint to responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,22 @@ module.exports = {
     container: {
       center: true,
     },
+    screens: {
+      
+      'xs': {'raw': '(max-width: 600px)'},
+
+      'sm': {'min': '600px'},
+      // => @media (min-width: 640px) { ... }
+
+      'md': {'min': '768px'},
+      // => @media (min-width: 768px) { ... }
+
+      'lg': {'min': '992px'},
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': {'min': '1200px'},
+      // => @media (min-width: 1280px) { ... }
+    }
   },
   variants: {
     extend: {},
